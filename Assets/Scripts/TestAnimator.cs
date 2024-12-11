9834999a8a8830ab8216a5cd6cb8c51552caf117
Andriy Zakryzhnuy <139996361+Kosheew@users.noreply.github.com>
2024-12-12 00:17:30 +0200

    Update Animation

--- Assets/Scripts/TestAnimator.cs
+++ Assets/Scripts/TestAnimator.cs
@@ -6,16 +6,20 @@ public class TestAnimator : MonoBehaviour
     private const string Horizontal = "Horizontal";
     
     private Animator animator;
-
+    private CharacterController characterController;
     private int _hashHorizontal;
     private int _hashVertical;
     
     private bool _running;
+    private bool _isCrouched;
+    
+    private Vector2 lastDirection = Vector2.down; // Початкова сторона, наприклад, вниз.
+    private Vector2 moveDirection;
     
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        
+        characterController = GetComponent<CharacterController>();
         _hashHorizontal = Animator.StringToHash("Horizontal");
         _hashVertical = Animator.StringToHash("Vertical");
     }
@@ -25,7 +29,38 @@ public class TestAnimator : MonoBehaviour
         float horizontal = Input.GetAxis(Horizontal);
         float vertical = Input.GetAxis(Vertical);
         
-        animator.SetFloat(_hashHorizontal, horizontal);
-        animator.SetFloat(_hashVertical, vertical);
+        moveDirection = new Vector2(horizontal, vertical);
+        
+        float rawHorizontal = Input.GetAxisRaw("Horizontal");
+        float rawVertical = Input.GetAxisRaw("Vertical");
+        
+        if (rawHorizontal != 0 || rawVertical != 0)
+        {
+            lastDirection = moveDirection.normalized;
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _isCrouched = !_isCrouched;
+            animator.SetTrigger("Crouched");
+        }
+        
+        animator.SetBool("IsCrouched", _isCrouched);
+        // Передаємо дані в Animator
+        animator.SetFloat(_hashHorizontal, moveDirection.x);
+        animator.SetFloat(_hashVertical, moveDirection.y);
+        animator.SetFloat("LastMoveX", lastDirection.x);
+        animator.SetFloat("LastMoveY", lastDirection.y);
+        
+        
+       // animator.SetFloat(_hashHorizontal, horizontal);
+       // animator.SetFloat(_hashVertical, vertical);
+        
+        characterController.Move(new Vector3(0, -9.81f, 0) * Time.deltaTime);
     }
 }
