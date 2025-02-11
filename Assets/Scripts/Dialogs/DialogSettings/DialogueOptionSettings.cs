c633c0a7bf94260b7fbce2b1e65044fc61e55269
Andriy Zakryzhnuy <139996361+Kosheew@users.noreply.github.com>
2025-02-11 19:46:48 +0200

    2. Converting

---
+++ Assets/Scripts/Dialogs/DialogSettings/DialogueOptionSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue Option", menuName = "Dialogue/DialogueOptionSettings")]
+public class DialogueOptionSettings: ScriptableObject
+{
+    [SerializeField] private ushort uniqueID;
+    [SerializeField, TextArea(3, 5)] private string sentence;
+    
+    public ushort UniqueID => uniqueID;
+    public string Sentence => sentence;
+}
